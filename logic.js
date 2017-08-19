efbee7daf52b7cea3ce6f2acbdb164a74b1593f7
Karan Joshi <karanjoshi31@live.com>
2017-08-18 20:02:45 -0500

    condensed logic.js code

--- logic.js
+++ logic.js
@@ -2,68 +2,24 @@ var inputString = "";
 var inputNum = "";
 var buttonList = ['oneButton','twoButton','threeButton','fourButton',
 'fiveButton','sixButton','sevenButton','eightButton','nineButton'];
-
-var num1 = document.getElementById(buttonList[0]);
-num1.addEventListener("click",function(){
-
-  clickButton(this);
-});
-
-var num2 = document.getElementById(buttonList[1]);
-num2.addEventListener("click",function(){
-
-  clickButton(this);
-});
-
-var num3 = document.getElementById(buttonList[2]);
-num3.addEventListener("click",function(){
-
-  clickButton(this);
-});
-
-var num4 = document.getElementById(buttonList[3]);
-num4.addEventListener("click",function(){
-
-  clickButton(this);
-});
-
-var num5 = document.getElementById(buttonList[4]);
-num5.addEventListener("click",function(){
-
-  clickButton(this);
-});
-
-var num6 = document.getElementById(buttonList[5]);
-num6.addEventListener("click",function(){
-
-  clickButton(this);
-});
-
-var num7 = document.getElementById(buttonList[6]);
-num7.addEventListener("click",function(){
-
-  clickButton(this);
-});
-
-var num8 = document.getElementById(buttonList[7]);
-num8.addEventListener("click",function(){
-
-  clickButton(this);
-});
-
-var num9 = document.getElementById(buttonList[8]);
-num9.addEventListener("click",function(){
-
-  clickButton(this);
-});
-
 var elementList = [];
 
+for(var i = 0; i<buttonList.length; i++){
+  var element = document.getElementById(buttonList[i]);
+  element.addEventListener("click",function(){
+    clickButton(this);
+  });
+
+  elementList.push(element);
+}
 
 
 function clickButton(element){
   buttonNum = element.innerText
   inputNum = inputNum+buttonNum;
   console.log("hello from button "+buttonNum);
-  console.log("input number "+inputNum);
+}
+
+function printInput(){
+  console.log(inputString);
 }
