8b5e3c051b8dfc8ac0c3f2a7ab3330ce848083b9
Karan Joshi <karanjoshi31@live.com>
2017-08-20 18:29:30 -0500

    completed logic for doing basic operations

--- logic.js
+++ logic.js
@@ -1,12 +1,20 @@
 var inputString = "";
 var inputNum = "";
-var buttonList = ['oneButton','twoButton','threeButton','fourButton',
+var buttonList = ['zeroButton','oneButton','twoButton','threeButton','fourButton',
 'fiveButton','sixButton','sevenButton','eightButton','nineButton'];
 var elementList = [];
 
+var operatorList = ['plusButton','minusButton','multiplyButton','divideButton'];
+
 var inputBox = document.getElementById('inputDisplay');
+var operationDisplay = document.getElementById('commands');
+
 
 console.log("Max input display length:",14);
+
+var commandOp = "";
+
+var inputNum2 = "";
 
 for(var i = 0; i<buttonList.length; i++){
   var element = document.getElementById(buttonList[i]);
@@ -50,4 +58,67 @@ function clearDigits(){
 
 function printInput(){
   console.log(inputString);
+}
+
+
+for(var i = 0; i<operatorList.length;i++){
+  var operation = document.getElementById(operatorList[i]);
+  operation.addEventListener("click",function(){
+    updateOperation(this);
+  });
+}
+
+function updateOperation(command){
+
+
+  if(operationDisplay.innerText.length>=0){
+      commandOp = command;
+
+      if(operationDisplay.innerText.length == 0){
+        inputNum2 = inputNum;
+        operationDisplay.innerText = inputNum + " "+command.innerText;
+        inputNum = "";
+        inputBox.innerText = "";
+      }
+  }
+
+}
+
+
+var equal = document.getElementById('equalsButton');
+equal.addEventListener("click",function(){
+  equalAmount();
+});
+
+
+function equalAmount(){
+  if(operationDisplay.innerText != ""){
+    console.log(inputNum);
+    console.log(commandOp.innerText);
+    console.log(inputNum2);
+
+    operationDisplay.innerText = "";
+    returnedResult = calculate(inputNum, commandOp.innerText, inputNum2);
+    inputNum = returnedResult;
+    inputBox.innerText = inputNum;
+    inputNum = inputNum.toString();
+    inputNum2 = "";
+  }
+
+}
+
+function calculate(num1, operation, num2){
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  var result;
+  if(operation === "+"){
+    result = num1 + num2;
+  }else if(operation === "-"){
+    result = num2 - num1;
+  }else if(operation === "x"){
+    result = num1 * num2;
+  }else if(operation === "/"){
+    result = num1 / num2;
+  }
+  return result;
 }
